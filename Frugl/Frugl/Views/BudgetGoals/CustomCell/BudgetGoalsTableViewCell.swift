03d7dcd275290dd9c833707859dc35fc402e02ea
Jake Gloschat <Gloschatbeats@gmail.com>
2023-04-26 15:54:30 -0600

    Itemization View
    Adds itemization view and add item modal view.

---
+++ Frugl/Frugl/Views/BudgetGoals/CustomCell/BudgetGoalsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BudgetGoalsTableViewCell.swift
+//  Frugl
+//
+//  Created by Jake Gloschat on 4/26/23.
+//
+
+import UIKit
+
+class BudgetGoalsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Frugl/Frugl/Views/Itemization/CustomCell/ItemizationTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemizationTableViewCell.swift
+//  Frugl
+//
+//  Created by Jake Gloschat on 4/26/23.
+//
+
+import UIKit
+
+class ItemizationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
