44c55bace67fc2f18ff11ca8e5a12a7ebacd9326
Jake Gloschat <Gloschatbeats@gmail.com>
2023-04-27 10:32:52 -0600

    Sign In
    adds code for sign in capabilities

--- Frugl/Frugl/Views/UserSignIn/SignInViewModel.swift
+++ Frugl/Frugl/Views/UserSignIn/SignInViewModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+struct SignInAccountViewModel {
+    
+    func signInAccount(email: String, password: String) {
+     
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let error {
+                print(error.localizedDescription)
+            }
+            
+            if let authResult {
+                let user = authResult.user
+                print(user.uid)
+            }
+        }
+    }
+}
