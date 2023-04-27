105e619ee830ea8287fdc086eb7b0db1aec0a43a
Jake Gloschat <Gloschatbeats@gmail.com>
2023-04-27 11:20:58 -0600

    Wires up create account view
    Adds ability to create account to firebase

--- Frugl/Frugl/Views/CreateAccount/CreateAccountViewController.swift
+++ Frugl/Frugl/Views/CreateAccount/CreateAccountViewController.swift
@@ -8,15 +8,28 @@
 import UIKit
 
 class CreateAccountViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    @IBOutlet weak var userEmailTextField: UITextField!
+    @IBOutlet weak var userPassWordTextField: UITextField!
+    @IBOutlet weak var userConfirmPasswordTextField: UITextField!
+    
+    
+    // MARK: - Properties
+    var viewModel: CreateAccountViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        viewModel = CreateAccountViewModel()
     }
     
-    // MARK: - Navigation
-  //  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-
-//    }
-    
+    // MARK: - Action
+    @IBAction func createAccountButtonTapped(_ sender: Any) {
+        guard let email = userEmailTextField.text,
+              let password = userPassWordTextField.text,
+              let confirmPassword = userConfirmPasswordTextField.text else { return }
+        
+        viewModel.createAccount(email: email, password: password, confirmPassword: confirmPassword)
+        self.navigationController?.popViewController(animated: true)
+    }
 }
