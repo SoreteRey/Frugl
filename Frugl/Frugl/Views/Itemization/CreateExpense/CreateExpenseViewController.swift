a3a1f7269d24bd3eb4d8e3ee6b5ad08e5a103d1b
Jake Gloschat <Gloschatbeats@gmail.com>
2023-05-02 09:36:38 -0600

    Adds Outlets and Actions
    Connected labels, textfields, and buttons on Expenses views.

--- Frugl/Frugl/Views/Itemization/CreateExpense/CreateExpenseViewController.swift
+++ Frugl/Frugl/Views/Itemization/CreateExpense/CreateExpenseViewController.swift
@@ -9,21 +9,24 @@ import UIKit
 
 class CreateExpenseViewController: UIViewController {
 
+    
+    // MARK: - Outlets
+    @IBOutlet weak var expenseNameTextField: UITextField!
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var optionalDueDateTextField: UITextField!
+    @IBOutlet weak var optionalFirstAlertTextField: UITextField!
+    @IBOutlet weak var optionalSecondAlertTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Actions
+    @IBAction func addButtonTapped(_ sender: Any) {
     }
-    */
-
+    
+    
 }
--- Frugl/Frugl/Views/Itemization/ItemizationViewController.swift
+++ Frugl/Frugl/Views/Itemization/ItemizationViewController.swift
@@ -9,8 +9,17 @@ import UIKit
 
 class ItemizationViewController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var budgetLabel: UILabel!
+    @IBOutlet weak var expenseTotalLabel: UILabel!
+    @IBOutlet weak var expensesTableView: UITableView!
+    @IBOutlet weak var expectedBalanceLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    
 }
