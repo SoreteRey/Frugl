258776209658fd127b506d1ee832332da362609a
Matthew Hill <matthill265@gmail.com>
2023-05-09 10:00:29 -0600

    CalendarViewModel
    Creates the calendar View Model. Adds the load expense funtion.

---
+++ Frugl/Frugl/Views/Calendar/CalendarViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  CalendarViewModel.swift
+//  Frugl
+//
+//  Created by Matthew Hill on 5/9/23.
+//
+
+import Foundation
+
+protocol CalendarViewModelDelegate: AnyObject {
+    func expensesLoadedSuccessfully()
+}
+
+class CalendarViewModel {
+    // MARK: - Properties
+    let service: FireBaseSyncable
+    weak var delegate: CalendarViewModelDelegate?
+    var expenses: [Expense] = []
+    
+    init(service: FireBaseSyncable = FirebaseService(), delegate: CalendarViewModelDelegate) {
+        self.service = service
+        self.delegate = delegate
+    }
+    
+    // MARK: - Functions
+    
+    func loadExpenses() {
+        service.loadExpense { [weak self] result in
+            switch result {
+                
+            case .success(let expenses):
+                self?.expenses = expenses
+                self?.delegate?.expensesLoadedSuccessfully()
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
