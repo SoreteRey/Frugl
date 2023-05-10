f5a452e4017a2924a4b3dbff2aee56893f04e4dc
sguiscardo <sdguiscardo@gmail.com>
2023-05-10 10:49:01 -0500

    Commiting to update to new Develop

--- Frugl/Frugl/Views/PieChart/PieChartViewController.swift
+++ Frugl/Frugl/Views/PieChart/PieChartViewController.swift
@@ -16,49 +16,45 @@ class PieChartViewController: UIViewController {
     @IBOutlet weak var pieChartTableView: UITableView!
     
     // MARK: - Properties
-    var viewModel = PieChartViewModel()
-    
-    // MARK: - Lifecycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        pieChartTableView.dataSource = self
-        
-        viewModel.fetchDataFromFirebase { [weak self] in
-            self?.pieChartView.slices = self?.viewModel.slices
-            self?.pieChartTableView.reloadData()
-            
-        }
-        
-        func viewDidAppear(_ animated: Bool) {
-            super.viewDidAppear(animated)
-            
-            pieChartView.animateChart()
-        }
-    }
-}
-extension PieChartViewController: UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.slices.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "pieChartCell", for: indexPath) as! PieChartTableViewCell
-        let slice = viewModel.slices[indexPath.row]
-        cell.configure(with: slice.percent)
-        return cell
-    }
-}
-//        pieChartView.slices = [
-//            Slice(percent: 0.1, color: UIColor.systemGreen),
-//            Slice(percent: 0.1, color: UIColor.systemOrange),
-//            Slice(percent: 0.3, color: UIColor.systemGray),
-//            Slice(percent: 0.2, color: UIColor.systemYellow),
-//            Slice(percent: 0.1, color: UIColor.systemBlue),
-//            Slice(percent: 0.1, color: UIColor.systemPurple),
-//            //            Slice(percent: 0.1, color: UIColor.systemGreen),
-//            //            Slice(percent: 0.1, color: UIColor.systemBlue),
-//            //            Slice(percent: 0.1, color: UIColor.systemRed)
+     var viewModel = PieChartViewModel()
+     
+     // MARK: - Lifecycle
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         
+         // Fetch data from Firebase and update the pie chart
+         fetchDataAndUpdatePieChart()
+     }
+     
+     func fetchDataAndUpdatePieChart() {
+         viewModel.fetchDataFromFirebase { [weak self] in
+             DispatchQueue.main.async {
+                 self?.updatePieChart()
+             }
+         }
+     }
+     
+     func updatePieChart() {
+         let slices = viewModel.slices
+         
+         pieChartView.slices = slices.map { slice in
+             return Slice(percent: slice.percent, color: slice.color)
+         }
+         
+         pieChartView.setNeedsDisplay()
+     }
+ }
+//extension PieChartViewController: UITableViewDataSource {
 //
-//        ]
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return viewModel.slices.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "pieChartCell", for: indexPath) as! PieChartTableViewCell
+//        let slice = viewModel.slices[indexPath.row]
+//        cell.configure(with: slice.percent)
+//        return cell
+//    }
+//}
+
